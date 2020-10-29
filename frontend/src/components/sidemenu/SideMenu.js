44fe40391cbadda6b816d1e4f8e6e6c06f0f4cfb
SamarthMohanty <samarth.mohanty5@gmail.com>
2020-10-29 19:55:50 +0530

    new

--- frontend/src/components/sidemenu/SideMenu.js
+++ frontend/src/components/sidemenu/SideMenu.js
@@ -4,11 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    }
   },
   searchField: {
     marginLeft: theme.spacing(1),
@@ -40,6 +45,10 @@ const Sidebar = ({ width, height, children }) => {
     }
   };
 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+};
+
   React.useEffect((width) => {
     setX(-width);
   }, []);
@@ -62,8 +71,20 @@ const Sidebar = ({ width, height, children }) => {
             transform: `translate(${width}px, 40vh)`
           }}
         ></button>
-        
         <div className="content">
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <div>
+            <TextField
+              id="standard-select-currency"
+              select
+              label="Filter"
+              onChange={handleChange}
+              // helperText="Please select your currency"
+            >
+            </TextField>
+          </div>
+        </form>
         
         {children}
         <TextField
